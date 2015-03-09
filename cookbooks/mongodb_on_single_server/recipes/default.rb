dd86a189e89a092cc94f478fe614253bee0bd1c4
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2015-03-09 22:04:17 +0900

    複数のmongodを起動

--- cookbooks/mongodb_on_single_server/recipes/default.rb
+++ cookbooks/mongodb_on_single_server/recipes/default.rb
@@ -13,7 +13,25 @@ include_recipe 'mongodb::mongo_gem'
 mongod_instance node['mongodb']['instance_name'] do
   mongodb_type 'mongod'
   port 27300
-  identifier 'sh3'
+  identifier 'sh0'
+  replicaset   node
+  enable_rest  node['mongodb']['config']['rest']
+  smallfiles   node['mongodb']['config']['smallfiles']
+end
+
+mongod_instance node['mongodb']['instance_name'] do
+  mongodb_type 'mongod'
+  port 27301
+  identifier 'sh1'
+  replicaset   node
+  enable_rest  node['mongodb']['config']['rest']
+  smallfiles   node['mongodb']['config']['smallfiles']
+end
+
+mongod_instance node['mongodb']['instance_name'] do
+  mongodb_type 'mongod'
+  port 27302
+  identifier 'sh2'
   replicaset   node
   enable_rest  node['mongodb']['config']['rest']
   smallfiles   node['mongodb']['config']['smallfiles']
