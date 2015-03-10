2de21b409a4e53ef4e98d23b46ddc3b9e772e161
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2015-03-10 15:17:15 +0900

    configsvrを追加

---
+++ cookbooks/mongodb-single-instance/definitions/configsvr.rb
@@ -0,0 +1,159 @@
+
+define :configsvr_instance,
+       :action        => [:enable, :start],
+       :notifies      => [] do
+
+  provider = 'mongod'
+
+  require 'ostruct'
+
+  new_resource = OpenStruct.new
+
+  new_resource.name                       = params[:name]
+  new_resource.replicaset                 = params[:replicaset]
+  new_resource.service_action             = params[:action]
+  new_resource.service_notifies           = params[:notifies]
+
+  new_resource.dbconfig_file_template     = node['mongodb']['dbconfig_file_template']
+  identifier = params[:identifier]
+  name = identifier ? 'configsvr-%s' % identifier : 'configsvr'
+  new_resource.dbconfig_file              = '/etc/%s.conf' % name
+
+  config = {}
+  config = config.merge(node['mongodb']['config'])
+  config['port'] = params[:port]
+  config['logpath'] = '/var/log/mongodb/%s.log' % name
+  config['dbpath'] = '/var/lib/mongodb/%s' % name
+  config['pidfilepath'] = '/var/run/mongodb/%s.pid' % name
+  config['configsvr']   = true
+  new_resource.config = config
+
+  new_resource.dbpath = config['dbpath']
+  new_resource.logpath = config['logpath']
+
+  new_resource.sysconfig_file             = node['mongodb']['sysconfig_file']
+  new_resource.sysconfig_file_template    = node['mongodb']['sysconfig_file_template']
+
+  new_resource.bind_ip                    = node['mongodb']['config']['bind_ip']
+  new_resource.cluster_name               = node['mongodb']['cluster_name']
+  new_resource.init_dir                   = node['mongodb']['init_dir']
+  new_resource.init_script_template       = node['mongodb']['init_script_template']
+  # new_resource.is_replicaset              = node['mongodb']['is_replicaset']
+  # new_resource.is_shard                   = node['mongodb']['is_shard']
+  # new_resource.is_mongos                  = node['mongodb']['is_mongos']
+  new_resource.mongodb_group              = node['mongodb']['group']
+  new_resource.mongodb_user               = node['mongodb']['user']
+  new_resource.replicaset_name            = node['mongodb']['config']['replSet']
+  new_resource.root_group                 = node['mongodb']['root_group']
+  # new_resource.shard_name                 = node['mongodb']['shard_name']
+  # new_resource.sharded_collections        = node['mongodb']['sharded_collections']
+  new_resource.sysconfig_file             = node['mongodb']['sysconfig_file']
+  new_resource.sysconfig_vars             = node['mongodb']['sysconfig']
+  new_resource.template_cookbook          = node['mongodb']['template_cookbook']
+  new_resource.ulimit                     = node['mongodb']['ulimit']
+  new_resource.reload_action              = node['mongodb']['reload_action']
+
+  if node['mongodb']['apt_repo'] == 'ubuntu-upstart'
+    new_resource.init_file = File.join(node['mongodb']['init_dir'], "%s.conf" % name  )
+    mode = '0644'
+  else
+    new_resource.init_file = File.join(node['mongodb']['init_dir'], name)
+    mode = '0755'
+  end
+
+  # default file
+  template new_resource.sysconfig_file do
+    cookbook new_resource.template_cookbook
+    source new_resource.sysconfig_file_template
+    group new_resource.root_group
+    owner 'root'
+    mode '0644'
+    variables(
+      :sysconfig => {
+        DAEMON: '/usr/bin/mongod',
+        DAEMON_USER: new_resource.user,
+        DAEMON_OPTS: "--config #{new_resource.dbconfig_file}",
+        CONFIGFILE: new_resource.dbconfig_file,
+        ENABLE_MONGODB: "yes"
+      }
+    )
+    notifies new_resource.reload_action, "service[#{new_resource.name}]"
+  end
+
+  # config file
+  template new_resource.dbconfig_file do
+    cookbook new_resource.template_cookbook
+    source new_resource.dbconfig_file_template
+    group new_resource.root_group
+    owner 'root'
+    variables(
+      :config => new_resource.config
+    )
+    helpers MongoDBConfigHelpers
+    mode '0644'
+    notifies new_resource.reload_action, "service[#{new_resource.name}]"
+  end
+
+  # log dir [make sure it exists]
+  if new_resource.logpath
+    directory File.dirname(new_resource.logpath) do
+      owner new_resource.mongodb_user
+      group new_resource.mongodb_group
+      mode '0755'
+      action :create
+      recursive true
+    end
+  end
+
+  # dbpath dir [make sure it exists]
+  directory new_resource.dbpath do
+    owner new_resource.mongodb_user
+    group new_resource.mongodb_group
+    mode '0755'
+    action :create
+    recursive true
+    not_if { new_resource.is_mongos }
+  end
+
+  # Reload systemctl for RHEL 7+ after modifying the init file.
+  execute 'mongodb-systemctl-daemon-reload' do
+    command 'systemctl daemon-reload'
+    action :nothing
+  end
+
+  # init script
+  template new_resource.init_file do
+    cookbook new_resource.template_cookbook
+    source new_resource.init_script_template
+    group new_resource.root_group
+    owner 'root'
+    mode mode
+    variables(
+      :provides =>       provider,
+      :dbconfig_file  => new_resource.dbconfig_file,
+      :sysconfig_file => new_resource.sysconfig_file,
+      :ulimit =>         new_resource.ulimit,
+      :bind_ip =>        new_resource.bind_ip,
+      :port =>           new_resource.port
+    )
+    notifies new_resource.reload_action, "service[#{new_resource.name}]"
+
+    if(platform_family?('rhel') && node['platform_version'].to_i >= 7)
+      notifies :run, 'execute[mongodb-systemctl-daemon-reload]', :immediately
+    end
+  end
+
+  # service
+  service new_resource.name do
+    provider Chef::Provider::Service::Upstart if node['mongodb']['apt_repo'] == 'ubuntu-upstart'
+    supports :status => true, :restart => true
+    action new_resource.service_action
+    new_resource.service_notifies.each do |service_notify|
+      notifies :run, service_notify
+    end
+      # we don't care about a running mongodb service in these cases, all we need is stopping it
+    ignore_failure true if new_resource.name == 'mongodb'
+  end
+
+
+end
--- cookbooks/mongodb-single-instance/recipes/default.rb
+++ cookbooks/mongodb-single-instance/recipes/default.rb
@@ -10,29 +10,21 @@
 include_recipe 'mongodb::install'
 include_recipe 'mongodb::mongo_gem'
 
+configsvr_instance node['mongodb']['instance_name'] do
+  port 27200
+end
+
 mongod_instance node['mongodb']['instance_name'] do
-  mongodb_type 'mongod'
   port 27300
   identifier 'sh0'
-  replicaset   node
-  enable_rest  node['mongodb']['config']['rest']
-  smallfiles   node['mongodb']['config']['smallfiles']
 end
 
 mongod_instance node['mongodb']['instance_name'] do
-  mongodb_type 'mongod'
   port 27301
   identifier 'sh1'
-  replicaset   node
-  enable_rest  node['mongodb']['config']['rest']
-  smallfiles   node['mongodb']['config']['smallfiles']
 end
 
 mongod_instance node['mongodb']['instance_name'] do
-  mongodb_type 'mongod'
   port 27302
   identifier 'sh2'
-  replicaset   node
-  enable_rest  node['mongodb']['config']['rest']
-  smallfiles   node['mongodb']['config']['smallfiles']
 end
